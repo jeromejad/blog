efe0ca74cd53fd15cb6693c340e93383a56065bb
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-07-19 16:11:49 +0530

    rhyaeshrws

--- database/conn.php
+++ database/conn.php
@@ -1,4 +1,4 @@
 <?php
 //database connection
-($GLOBALS["___mysqli_ston"] = mysqli_connect("localhost","root","","blog_admin_db"));  //host,user,password,database
+($GLOBALS["___mysqli_ston"] = mysqli_connect("bcupibzywnva4fhilcxr-mysql.services.clever-cloud.com","uixdfpp1egrv5bdz","BEcOK7MBhTLYUC8TctW5","bcupibzywnva4fhilcxr"));  //host,user,password,database
 ?>
--- database/db.php
+++ database/db.php
@@ -1,5 +1,5 @@
 <?php
-	$database_username = 'root';//user name
-	$database_password = '';//password
-	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
+	$database_username = 'uixdfpp1egrv5bdz';//user name
+	$database_password = 'BEcOK7MBhTLYUC8TctW5';//password
+	$pdo_conn = new PDO( 'mysql:host=bcupibzywnva4fhilcxr-mysql.services.clever-cloud.com;dbname=bcupibzywnva4fhilcxr', $database_username, $database_password );
 ?>
