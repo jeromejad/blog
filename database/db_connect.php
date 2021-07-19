80cad22a6bd353d3fc575ff708dd60ff6c552018
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-07-19 16:13:59 +0530

    Update db_connect.php

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("bcupibzywnva4fhilcxr-mysql.services.clever-cloud.com","uixdfpp1egrv5bdz","BEcOK7MBhTLYUC8TctW5","bcupibzywnva4fhilcxr"));  //host,user,password,database
+$con=mysqli_connect("bcupibzywnva4fhilcxr-mysql.services.clever-cloud.com","uixdfpp1egrv5bdz","BEcOK7MBhTLYUC8TctW5","bcupibzywnva4fhilcxr");  //host,user,password,database
 // Check connection
 if (mysqli_connect_errno())
   {
